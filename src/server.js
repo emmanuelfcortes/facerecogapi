84470220d8c75711b3c441020135bb64c5e7cabb
emmanuelcortes <emmanuelcortes@users.noreply.github.com>
2018-08-21 13:34:16 -0300

    adicionado process.env.PORT ao server.js

--- src/server.js
+++ src/server.js
@@ -73,8 +73,8 @@ app.put("/entries", (req, resp)=> { entries.handleEntries(req,resp,db)
 	
 })
 	
-app.listen(3000, ()=>{
-	console.log("Its working on port 3000...");
+app.listen(process.env.PORT || 3000, ()=>{
+	console.log("Its working on port "+process.env.PORT+" ...");
 });
 
 /*
