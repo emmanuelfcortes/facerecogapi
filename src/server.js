5dd24c6e49630af160aa897b572dc040e1c2b9e2
emmanuelcortes <emmanuelcortes@users.noreply.github.com>
2018-08-30 12:21:32 -0300

    Modificação para apagar as tabelas

--- src/server.js
+++ src/server.js
@@ -37,9 +37,9 @@ app.use(bodyParser.json());// middleware that transform the frontend recept data
 app.use(cors());
 
 // criando os bds USERS  e LOGIN =======================================
-/*db.schema.dropTable('users');
-db.schema.dropTable('login');*/
-db.schema.createTable('users', users => {
+db.schema.dropTable('users').then(resp=>console.log("===================>>>DROP THE TABLE USER<<<<==========="));
+db.schema.dropTable('login').then(resp=>console.log("===================>>>DROP THE TABLE USER<<<<==========="));
+/*db.schema.createTable('users', users => {
 	users.increments('id').primary().unique();
 	users.string('name',100);
 	users.string('password',30);
@@ -56,7 +56,7 @@ db.schema.createTable('login', login=>{
 	login.string('email');
 	login.string('passwordhash');
 }).then(resp=>console.log("===============>>>>>>>LOGIN WAS CREATED<<<<<<================",resp))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err));*/
 
 //=====================================================================
 
